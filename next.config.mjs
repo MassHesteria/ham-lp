84038ea964539e6e6441665ddd8f9f6f780e43e3
MassHesteria <brad.hester@gmail.com>
2024-06-19 14:28:56 -0500

    middleware for home page

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Cache-Control',
-            value: 'public, s-maxage=1200, max-age=1200, stale-while-revalidate=30',
+            value: 'public, s-maxage=1200, max-age=1200, stale-while-revalidate=60',
             //value: 'public, s-maxage=31536000, max-age=31536000, immutable',
           },
         ],
