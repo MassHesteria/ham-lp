ab5daf6ee5c6de3dcc554c865e339e35a6d465fc
MassHesteria <brad.hester@gmail.com>
2024-06-19 07:18:52 -0500

    share route cache config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/share/:slug*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, s-maxage=60, max-age=60, stale-while-revalidate=30',
+          },
+        ],
+      },
+    ]
+  }
+};
 
 export default nextConfig;
