48587831a875bcd151f66a0ce143bbcbd671e4f5
MassHesteria <brad.hester@gmail.com>
2024-06-19 21:02:43 -0500

    inline background

--- app/page/route.tsx
+++ app/page/route.tsx
@@ -1,9 +1,48 @@
+import { ImageResponse } from 'next/og';
 import { NextRequest, NextResponse } from "next/server"
 import { getHostName } from "../frames"
 import sharp from "sharp"
 
 const getBackground = async (username: string) => {
-  const data = await fetch(getHostName() + `/background?u=${username}&v=3`)
+  const data = new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 40,
+          color: '#8be9fd',
+          background: '#282a36',
+          width: '100%',
+          height: '100%',
+          justifyContent: 'center',
+          paddingTop: '16px',
+          display: 'flex',
+          flexDirection: 'column'
+        }}
+      >
+        <div style={{
+          display: 'flex',
+          justifyContent: 'center',
+          height: '93.5%'
+          }}>
+          <span>Ham LPs of @{username}</span>
+        </div>
+        <div style={{
+          display: 'flex',
+          justifyContent: 'flex-end',
+          fontSize: 16,
+          paddingRight: '6px',
+          paddingBottom: '16px',
+          color: '#6272a4'
+          }}>
+          by @masshesteria
+        </div>
+      </div>
+    ),
+    {
+      width: 764,
+      height: 400,
+    },
+  );
   return await data.arrayBuffer()
 }
 
